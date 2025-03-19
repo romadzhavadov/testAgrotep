a17c5d6b0c7ef152ed8446d1cdddbba17dd04b6b
romadzhavadov <r.dzhavadov12@gmail.com>
2025-03-19 21:57:58 +0200

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,70 @@
-/*!
-* Start Bootstrap - Heroic Features v5.0.6 (https://startbootstrap.com/template/heroic-features)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-heroic-features/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+let selectedChars = new Set();
 
-// call to action random colors
+const submitButton = document.querySelector(".submit-btn");
+submitButton.addEventListener("click", displayText);
 
-const button = document.getElementById("callToAction");
-const newCaption = document.getElementById("newCaption");
-const animatedText = document.getElementById("animatedText");
-const colors = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3'];
+function displayText() {
+    const input = document.getElementById("textInput").value;
+    const output = document.getElementById("output");
+    output.innerHTML = "";
+    
+    input.split("").forEach((char, index) => {
+        let span = document.createElement("span");
+        span.textContent = char;
+        span.classList.add("char");
+        span.setAttribute("draggable", "true");
+        span.setAttribute("data-index", index);
+        
+        span.addEventListener("click", toggleSelection);
+        span.addEventListener("dragstart", handleDragStart);
+        span.addEventListener("dragover", handleDragOver);
+        span.addEventListener("drop", handleDrop);
+        
+        output.appendChild(span);
+    });
+}
 
-button.addEventListener('click', () => {
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    button.style.backgroundColor = randomColor;
-
-    const fetchCatFact = async () => {
-        try {
-            const res = await fetch('https://catfact.ninja/fact')
-            const data = await res.json();
-            console.log(data)
-            newCaption.textContent = data.fact;
-        } catch (err) {
-            console.error('Error fact', err)
+function toggleSelection(event) {
+    if (event.ctrlKey) {
+        event.target.classList.toggle("selected");
+        if (selectedChars.has(event.target)) {
+            selectedChars.delete(event.target);
+        } else {
+            selectedChars.add(event.target);
         }
     }
-
-    animatedText.classList.add('fade-out');
-    setTimeout(() => {
-        animatedText.textContent = ''; 
-    }, 1000);
-
-    fetchCatFact()
-})
-
-// drag and drop
-
-const draggableElements = document.querySelectorAll('#draggableContainer .col-lg-6');
-
-draggableElements.forEach(element => {
-    element.addEventListener('dragstart', dragStart);
-    element.addEventListener('dragover', dragOver);
-    element.addEventListener('drop', drop);
-});
-
-function dragStart(e) {
-    e.dataTransfer.setData('text/plain', e.target.outerHTML);
-    e.dataTransfer.effectAllowed = 'move';
-    e.target.classList.add('dragging');
 }
 
-function dragOver(e) {
-    e.preventDefault();
+function handleDragStart(event) {
+    event.dataTransfer.setData("text/plain", event.target.dataset.index);
 }
 
-function drop(e) {
-    e.preventDefault();
-    const draggingElement = document.querySelector('.dragging');
-    if (e.target.closest('.col-lg-6')) {
-        const targetHtml = e.target.closest('.col-lg-6').outerHTML;
-        e.target.closest('.col-lg-6').outerHTML = draggingElement.outerHTML; 
-        draggingElement.outerHTML = targetHtml; 
-        initDraggableElements();
-        changeSquareColors();
+function handleDragOver(event) {
+    event.preventDefault();
+}
+
+function handleDrop(event) {
+    event.preventDefault();
+    const draggedIndex = event.dataTransfer.getData("text/plain");
+    const draggedElement = document.querySelector(`[data-index='${draggedIndex}']`);
+    const targetElement = event.target;
+
+    if (draggedElement && targetElement && draggedElement !== targetElement) {
+        let parent = targetElement.parentNode;
+        let draggedClone = draggedElement.cloneNode(true);
+        let targetClone = targetElement.cloneNode(true);
+
+        parent.replaceChild(draggedClone, targetElement);
+        parent.replaceChild(targetClone, draggedElement);
+
+        draggedClone.addEventListener("click", toggleSelection);
+        draggedClone.addEventListener("dragstart", handleDragStart);
+        draggedClone.addEventListener("dragover", handleDragOver);
+        draggedClone.addEventListener("drop", handleDrop);
+
+        targetClone.addEventListener("click", toggleSelection);
+        targetClone.addEventListener("dragstart", handleDragStart);
+        targetClone.addEventListener("dragover", handleDragOver);
+        targetClone.addEventListener("drop", handleDrop);
     }
 }
-
-function changeSquareColors() {
-    const squares = document.querySelectorAll('.feature'); 
-    squares.forEach(square => {
-        square.classList.remove('bg-primary');
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
-        square.style.backgroundColor = randomColor; 
-    });
-}
-
-function initDraggableElements() {
-    const draggableElements = document.querySelectorAll('#draggableContainer .col-lg-6');
-    draggableElements.forEach(element => {
-        element.addEventListener('dragstart', dragStart);
-        element.addEventListener('dragover', dragOver);
-        element.addEventListener('drop', drop);
-        element.classList.remove('dragging'); 
-    });
-}
-
-initDraggableElements();
